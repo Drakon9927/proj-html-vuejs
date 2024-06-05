9b2e77077d88e929a437cd60201c6c332d64891d
Antonino <drakon9927@libero.it>
2024-06-05 14:01:53 +0200

    animations updates

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,77 +10,68 @@
       </div>
       <div class="row text-left g-0 carousel">
         <div class="col-2 mb-4" v-for="(pizza, index) in pizzas" :key="index">
-          <div class="card no-border slide">
-            <img :src="pizza.imageUrl" class="card-img-top" alt="Image of pizza">
+            <div class="card no-border slide">
+              <div class="img-container">
+                <img :src="pizza.imageUrl" class="card-img-top" alt="Image of pizza">
+                <span v-if="pizza.name === 'Bismark'" class="badge-circle orangebg">-14%</span>
+                <span v-if="pizza.name === 'Valdostana'" class="badge-circle">SOLD</span>
+              </div>
             <div class="card-body">
               <h5 class="card-title pizza-text fw-bold">{{ pizza.name }}</h5>
               <p class="card-text price-text fw-bold" v-html="formatPrice(pizza.price)"></p>
+              <div class="button-group">
+                <button class="btn btn-danger text-white me-2 fs-bold">ADD TO CART</button>
+                <button class="btn btn-secondary text-black fs-bold">QUICK VIEW</button>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
 
-    <!-- Container 2 -->
     <div class="container-fluid full-width-container my-3 mystarbg">
       <div class="row g-0">
         <div class="col-6 bigpadding">
           <p class="fs-6 text-danger">MADE WITH LOVE</p>
           <h2 class="text-white mb-3">DELISH PIZZA DEALS</h2>
-
           <div class="row g-0 fs-4">
             <div class="div text-white w-auto">
               <span class="text-danger">02</span><br>
               <span>NOV</span>
             </div>
-
             <div class="div text-white w-auto ms-4">
               <span>TRADITIONAL NEAPOLITAN PIES IN</span> <br>
               <span>KYOTO PIZZA MERCATO</span> <br>
               <span class="text-danger fs-6">204 E. Pizzetta Tommaso</span>
-
             </div>
           </div>
-
           <hr class="dotted-hr w-75">
-
           <div class="row g-0 fs-4">
             <div class="div text-white w-auto">
               <span class="text-danger">03</span><br>
               <span>NOV</span>
             </div>
-
             <div class="div text-white w-auto ms-4">
               <span>TERAZZA PATIO DINING SPACE</span> <br>
               <span>OPENING THIS WEEKEND</span> <br>
               <span class="text-danger fs-6">204 E. Pizzetta Tommaso</span>
-
             </div>
           </div>
-
           <hr class="dotted-hr w-75">
-
           <div class="row g-0 fs-4">
             <div class="div text-white w-auto">
               <span class="text-danger">05</span><br>
               <span>NOV</span>
             </div>
-
             <div class="div text-white w-auto ms-4">
               <span>SIENNA PRIVATE DINING ROOM WITH</span> <br>
               <span>STEPHANE BRUNN</span> <br>
               <span class="text-danger fs-6">204 E. Pizzetta Tommaso</span>
-
             </div>
           </div>
-
         </div>
-
-        <div class="col-6 myimgbg">
-
-        </div>
+        <div class="col-6 myimgbg"></div>
       </div>
-
     </div>
   </main>
 </template>
@@ -115,62 +106,91 @@ export default {
 </script>
 
 <style>
-.full-width-container {
-  width: 100% !important;
-  padding-left: 0 !important;
-  padding-right: 0 !important;
-}
+  .full-width-container {
+    width: 100% !important;
+    padding-left: 0 !important;
+    padding-right: 0 !important;
+  }
+  .no-border {
+    border: none;
+  }
+  .card-img-top {
+    height: 200px;
+    width: 200px;
+  }
+  .mystarbg {
+    background-image: url(/public/img/cielostellato.PNG);
+  }
+  .bigpadding {
+    padding: 100px;
+  }
+  .dotted-hr {
+    border: 0;
+    border-top: 3px dotted yellow;
+    margin: 2rem 0;
+  }
+  .myimgbg {
+    background-image: url(/public/img/h3-img-5a.jpg);
+    background-size: cover;
+  }
+  .carousel {
+    overflow: hidden;
+    position: relative;
+    white-space: nowrap;
+  }
+  .slide {
+    display: inline-block;
+    transition: transform 1s ease;
+  }
+  .carousel .col-2 {
+    display: inline-block;
+    float: none;
+    vertical-align: top;
+  }
+  .price-text {
+    color: red; 
+  }
+  .pizza-text {
+    color: #B7903C;
+  }
+  .card:hover .button-group {
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+  }
+  .button-group {
+    display: none;
+    margin-top: 10px;
+  }
+  .button-group button {
+    padding: 15px;
+  }
 
-.no-border {
-  border: none;
-}
-
-.card-img-top {
-  height: 200px;
-  width: 200px;
-}
-
-.mystarbg {
-  background-image: url(/public/img/cielostellato.PNG);
-}
-
-.bigpadding {
-  padding: 100px;
-}
-
-.dotted-hr {
-  border: 0;
-  border-top: 3px dotted yellow;
-  margin: 2rem 0;
-}
-
-.myimgbg {
-  background-image: url(/public/img/h3-img-5a.jpg);
-  background-size: cover;
-}
-
-.carousel {
-  overflow: hidden;
+  /* Sold e -14% */
+  .img-container {
   position: relative;
-  white-space: nowrap;
-}
-
-.slide {
   display: inline-block;
-  transition: transform 1s ease;
 }
 
-.carousel .col-2 {
-  display: inline-block;
-  float: none;
-  vertical-align: top;
+.badge-circle {
+  position: absolute;
+  top: 20%; 
+  right: 0%; 
+  transform: translate(-50%, -50%); 
+  background-color: red; 
+  color: white; 
+  border-radius: 50%; 
+  padding: 10px; 
+  font-size: 0.8em; 
+  font-weight: bold;
+  width: 80px; 
+  height: 80px; 
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.price-text {
-  color: red; 
-}
-
-.pizza-text{
-  color: #B7903C;
+.orangebg{
+  background-color: #FFA323;
 }
 </style>
